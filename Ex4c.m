6d2579d48f5eea1c94ea95d47b22ce0c6c88ea84
Nour <74349478+bsnoor@users.noreply.github.com>
2023-06-12 20:27:56 +0300

    Create Ex4c.m

---
+++ Ex4c.m
@@ -0,0 +1,35 @@
+function y=bisection(x)
+y=3*x.^4+8*x.^3+6*x.^2-10;
+
+
+clear,clc;
+x=-2:0.001:2;
+y=bisection(x);
+figure(1)
+plot(x,y);
+grid;
+a=0;
+b=1;
+eps=10^(-6);
+mid=b-a;
+m=1;
+format long
+while mid >=eps
+    x=(a+b)/2;
+    c(m)=x;
+    if bisection(a)*bisection(b)<0
+        b=x;
+    else
+        a=x;
+    end
+    m=m+1;
+    mid=b-a;
+end
+disp('number of iterations: ')
+disp(m);
+disp('the result is :')
+c(m-1)
+
+figure(2)
+plot(c,'-r')
+
